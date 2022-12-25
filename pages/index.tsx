97b8624928aeffa48d0cf1468b89673cbae5d2ff
shashank rai <shashank.rai00331@gmail.com>
2022-12-26 00:09:38 +0530

    eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+
 import Nav from "../Comps/Nav";
 import Homes from "../Comps/Home";
 import About from "../Comps/About";
@@ -13,7 +14,7 @@ export default function Home() {
     <div>
       <Head>
         <title>Portfolio</title>
-        <script src='/function.js'/>
+        <script src='/function.js' defer/>
         <link
           rel='stylesheet'
           href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'
@@ -23,10 +24,10 @@ export default function Home() {
           href='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css'
         />
         <link rel='icon' href='./images/me2.png' type='image/x-icon'></link>
-        <script src='https://code.jquery.com/jquery-3.5.1.min.js'></script>
-        <script src='https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js'></script>
-        <script src='https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js'></script>
-        <script src='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js'></script>
+        <script src='https://code.jquery.com/jquery-3.5.1.min.js' defer></script>
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js' defer></script>
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js' defer></script>
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js' defer></script>
       </Head>
       <Nav />
       <Homes />
